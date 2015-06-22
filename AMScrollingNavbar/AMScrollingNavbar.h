2e6960295ac2db3c1b4dbba35c5eade7cff61deb
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-06-22 20:22:24 +1000

    Add support for Carthage

---
+++ AMScrollingNavbar/AMScrollingNavbar.h
@@ -0,0 +1,17 @@
+//
+//  AMScrollingNavbar.h
+//  AMScrollingNavbar
+//
+//  Created by Nicholas Tian on 22/06/2015.
+//  Copyright © 2015 AMScrollingNavbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMScrollingNavbar.
+FOUNDATION_EXPORT double AMScrollingNavbarVersionNumber;
+
+//! Project version string for AMScrollingNavbar.
+FOUNDATION_EXPORT const unsigned char AMScrollingNavbarVersionString[];
+
+#import <AMScrollingNavbar/UIViewController+ScrollingNavbar.h>
