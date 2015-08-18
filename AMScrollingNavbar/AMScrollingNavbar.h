a77b9f599bd824dbe9343428306b417f8553779d
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-08-18 18:49:27 +0200

    Fix carthage support

---
+++ AMScrollingNavbar/AMScrollingNavbar.h
@@ -0,0 +1,19 @@
+//
+//  AMScrollingNavbar.h
+//  AMScrollingNavbar
+//
+//  Created by Andrea Mazzini on 18/08/15.
+//  Copyright (c) 2015 Fancy Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMScrollingNavbar.
+FOUNDATION_EXPORT double AMScrollingNavbarVersionNumber;
+
+//! Project version string for AMScrollingNavbar.
+FOUNDATION_EXPORT const unsigned char AMScrollingNavbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMScrollingNavbar/PublicHeader.h>
+
+
