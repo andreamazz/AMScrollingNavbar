8614dc6ed0a317c40bec8de8903f120bb66886dc
Sammy Gutierrez <sammy.gutierrez@me.com>
2016-08-30 12:16:14 -0500

    Update demo project to Swift 3, upgrade Cocoapods version

--- Demo/ScrollingNavbarDemo/AppDelegate.swift
+++ Demo/ScrollingNavbarDemo/AppDelegate.swift
@@ -13,11 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-//        UINavigationBar.appearance().translucent = false
-        UINavigationBar.appearance().tintColor = .white()
-        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white()]
-        return true
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
     }
 
 }
