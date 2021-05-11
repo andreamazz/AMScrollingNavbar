andreamazz/AMScrollingNavbar

commit 4d14f0b7756269d34030a6153d47f24662666119

Update ScrollingNavigationController.swift
Fixed #398 by using IOS 13+ APIs to update titleTextAttributes across all possible NavigationBar appearance modes.

Source/ScrollingNavigationController.swift
@@ -648,6 +648,19 @@ open class ScrollingNavigationController: UINavigationController, UIGestureRecog
     let alpha = 1 - percentage
     
     // Hide all the possible titles
+    if #available(iOS 13.0, *) {
+        if let color = navigationBar.scrollEdgeAppearance?.titleTextAttributes [NSAttributedString.Key.foregroundColor] as? UIColor {
+            navigationBar.scrollEdgeAppearance?.titleTextAttributes [NSAttributedString.Key.foregroundColor] = color.withAlphaComponent(alpha)
+        }
+        
+        if let color = navigationBar.standardAppearance.titleTextAttributes [NSAttributedString.Key.foregroundColor] as? UIColor {
+            navigationBar.standardAppearance.titleTextAttributes [NSAttributedString.Key.foregroundColor] = color.withAlphaComponent(alpha)
+        }
+        
+        if let color = navigationBar.compactAppearance?.titleTextAttributes [NSAttributedString.Key.foregroundColor] as? UIColor {
+            navigationBar.compactAppearance?.titleTextAttributes [NSAttributedString.Key.foregroundColor] = color.withAlphaComponent(alpha)
+        }
+    }
     navigationItem.titleView?.alpha = alpha
     navigationBar.tintColor = savedNavBarTintColor?.withAlphaComponent(alpha)
     navigationItem.leftBarButtonItem?.tintColor = navigationItem.leftBarButtonItem?.tintColor?.withAlphaComponent(alpha)

commit 8726344553eba019b8aba1b08a839c5b773c3e3e

Remove bug introduced in #399

Source/ScrollingNavigationController.swift
@@ -648,15 +648,16 @@ open class ScrollingNavigationController: UINavigationController, UIGestureRecog
     // Change the alpha channel of every item on the navbr
     let alpha = 1 - percentage
     
-    // Hide all the possible tgit push origin masteritles (See #398)
+    // Hide all the possible titles (See #398)
     if #available(iOS 13.0, *) {
       if let color = navigationBar.scrollEdgeAppearance?.titleTextAttributes [NSAttributedString.Key.foregroundColor] as? UIColor {
         navigationBar.scrollEdgeAppearance?.titleTextAttributes [NSAttributedString.Key.foregroundColor] = color.withAlphaComponent(alpha)
       }
 
-      if let color = navigationBar.standardAppearance.titleTextAttributes [NSAttributedString.Key.foregroundColor] as? UIColor {
-        navigationBar.standardAppearance.titleTextAttributes [NSAttributedString.Key.foregroundColor] = color.withAlphaComponent(alpha)
-      }
+      // NOTE: this breaks the tintColor, it's disabled for the time being
+//      if let color = navigationBar.standardAppearance.titleTextAttributes [NSAttributedString.Key.foregroundColor] as? UIColor {
+//        navigationBar.standardAppearance.titleTextAttributes [NSAttributedString.Key.foregroundColor] = color.withAlphaComponent(alpha)
+//      }
       
       if let color = navigationBar.compactAppearance?.titleTextAttributes [NSAttributedString.Key.foregroundColor] as? UIColor {
         navigationBar.compactAppearance?.titleTextAttributes [NSAttributedString.Key.foregroundColor] = color.withAlphaComponent(alpha)